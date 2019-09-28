7d891efb994c6129925b4a48c2122f1bd9ac5031
Thyago Oliveira <taothyago@gmail.com>
2019-09-28 15:42:16 -0300

    :construction: Update header file

--- Tree.h
+++ Tree.h
@@ -1,1 +1,10 @@
 typedef struct tree Tree;
+
+Tree * create();
+void destroyTree(Tree * t);
+void add(Tree * tree, char * name);
+int count_nodes(Tree * tree);
+int count_leaf(Tree * tree);
+int height(Tree * tree);
+int search(Tree * tree, char * name);
+void printTree(Tree * tree);
